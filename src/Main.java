a3136c6c101b19b2102631644c6bbccf9a8eb980
Byron Manuel <99151273+Bbeeron@users.noreply.github.com>
2025-03-13 14:20:43 -0400

    Initialized Array

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public int partition(int[] arr, int low, int high){
+        return -1;
+    }
+
+    public void swap(int[] arr, int i, int j){
+        ;
+    }
+
+    public void quickSort(int[] arr, int low, int high){
+        ;
+    }
+
+    public static void main(String[] args){
+        Random rand = new Random();
+        int[] arr = new int[10];
+
+        for(int k = 0; k < arr.length; k++){
+            int num = rand.nextInt(10);
+            arr[k] = num;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        int low = 0;
+        int high = 9;
+        
+    }
+
+}
