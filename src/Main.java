2c1944fb6c3ab8f61803fd9a7bf7ba50d8c0d1f6
Byron Manuel <99151273+Bbeeron@users.noreply.github.com>
2025-03-14 14:23:53 -0400

    Worked on partitioning

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ import java.util.*;
 
 public class Main {
 
-    public int partition(int[] arr, int low, int high){
+    public static int partition(int[] arr, int low, int high){
+        int midpoint = ( high + low ) / 2;
+        if (arr[low] > midpoint){
+            swap(arr, low, midpoint);
+        }
+        else if (arr[high] < midpoint){
+            swap(arr, high, midpoint);
+        }
+
+        System.out.println(Arrays.toString(arr));
+
         return -1;
+//        return -1;
     }
 
-    public void swap(int[] arr, int i, int j){
-        ;
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    public void quickSort(int[] arr, int low, int high){
+    public static void quickSort(int[] arr, int low, int high){
         ;
     }
 
@@ -19,15 +32,13 @@ public class Main {
         int[] arr = new int[10];
 
         for(int k = 0; k < arr.length; k++){
-            int num = rand.nextInt(10);
+            int num = rand.nextInt(arr.length);
             arr[k] = num;
         }
 
         System.out.println(Arrays.toString(arr));
 
-        int low = 0;
-        int high = 9;
-        
+        System.out.println(partition(arr, 0, arr.length));
     }
 
 }
