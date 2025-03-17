82921bd0826f44b00c0703bf84765f1095107f50
Byron Manuel <99151273+Bbeeron@users.noreply.github.com>
2025-03-17 14:19:53 -0400

    Nothing

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,23 @@ import java.util.*;
 public class Main {
 
     public static int partition(int[] arr, int low, int high){
-        int midpoint = ( high + low ) / 2;
-        if (arr[low] > midpoint){
-            swap(arr, low, midpoint);
+        int midpoint = (low + high) / 2;
+        int numMid = arr[midpoint];
+        int tempNum = arr[high];
+        arr[high] = numMid;
+        arr[midpoint] = tempNum;
+        System.out.println(Arrays.toString(arr));
+
+        if (arr[high - 1] < numMid) {
+            swap(arr, high - 1, midpoint);
         }
-        else if (arr[high] < midpoint){
-            swap(arr, high, midpoint);
+        if (low > numMid){
+            swap(arr, low, midpoint);
         }
 
         System.out.println(Arrays.toString(arr));
 
         return -1;
-//        return -1;
     }
 
     public static void swap(int[] arr, int i, int j){
@@ -28,17 +33,15 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Random rand = new Random();
-        int[] arr = new int[10];
+        int[] arr = {2, 5, 6, 3, 1, 8, 10, 9, 4, 7};
 
-        for(int k = 0; k < arr.length; k++){
-            int num = rand.nextInt(arr.length);
-            arr[k] = num;
-        }
+//        for(int k = 0; k < arr.length; k++){
+//            arr[k] = k+1;
+//        }
 
         System.out.println(Arrays.toString(arr));
 
-        System.out.println(partition(arr, 0, arr.length));
+        System.out.println(partition(arr, 0, arr.length-1));
     }
 
 }
