94afbe57599e885eb1789508887e02c14e20d461
Beedro <100849675+Beedro@users.noreply.github.com>
2025-03-26 14:25:04 -0400

    Worked with Mr.C

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,47 @@
 import java.util.*;
 
-public class  Main {
+public class Main {
 
-    public static int partition(int[] arr, int low, int high){
+    public static int partition(int[] arr, int low, int high) {
         int mid = (high + low) / 2;
         int pivot = arr[mid];
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Pivot is: " + pivot);
         swap(arr, mid, high);
-        System.out.println(Arrays.toString(arr));
-        int compare = -1;
-        for (int step = 0; step < high; step++){
+        System.out.println("Array after midpoint swap: " + Arrays.toString(arr));
+        int high_i = high - 1;
 
-            if (arr[step] < pivot){
-                compare++;
-                swap(arr, step, compare);
+        for(int low_i = low; low_i < high; low_i++) {
+            if (arr[low_i] > pivot) {
+                high_i--;
+                System.out.println(arr[low_i] + " is greater than " + pivot);
+                System.out.println(arr[high_i] + " is less than " + pivot);
+                swap(arr, low_i, high_i);
+                System.out.println("Array after low/high swap: " + Arrays.toString(arr));
+
+            }
+
+            System.out.println("low_i " + low_i + " high_i" + high_i);
+        }
+        int i;
+        for (i = low; i < high; i++) {
+            if (arr[i] > pivot){
+                System.out.println(i);
+                swap(arr, i, high);
             }
         }
-        System.out.println(Arrays.toString(arr));
-        swap(arr, compare+1, high);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(arr[compare+1]);
-
-       return compare+1;
+        return i;
+//
+//
+//        for(int step = 0; step < high; step++){
+//
+//            if (arr[step] < pivot){
+//                compare++;
+//                swap(arr, step, compare);
+//            }
+//        }
+//        swap(arr, compare+1, high);
+//        System.out.println(arr[compare+1]);
+//       return compare+1;
     }
 
     public static void swap(int[] arr, int i, int j){
@@ -31,13 +51,13 @@ public class  Main {
     }
 
     public static void quickSort(int[] arr, int low, int high){
-        int pIndex = partition(arr, low, high);
-        if (pIndex == 0){
-            System.out.println(Arrays.toString(arr));
-        }
-        else{
-            quickSort(arr, low, pIndex-1);
-            quickSort(arr, pIndex+1, high);
+        if(low < high)
+        {
+            int pIndex = partition(arr, low, high);
+            System.out.println("pIndex is " + pIndex);
+            quickSort(arr, pIndex + 1, high);
+            quickSort(arr, low, pIndex - 1);
+
         }
     }
 
